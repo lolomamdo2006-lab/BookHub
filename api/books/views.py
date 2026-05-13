79adbd7062555a00bc561b06bb7c98215d311e03
Yara Ayman <yaramhran2006@gmail.com>
2026-05-13 23:35:34 +0300

    Add requirements and finalize backend

--- api/books/views.py
+++ api/books/views.py
@@ -1,12 +1,20 @@
-from rest_framework import viewsets, filters, status
-from rest_framework.response import Response
+from rest_framework import viewsets, filters
 from .models import Book
+# , BorrowedBook 
 from .serializers import BookSerializer
-from rest_framework.permissions import AllowAny
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'author', 'category']
+
+    
+    @action(detail=False, methods=['get'])
+    def borrowed(self, request):
+        borrowed_books = Book.objects.filter(is_available=False)
+        serializer = self.get_serializer(borrowed_books, many=True)
+        return Response(serializer.data)
